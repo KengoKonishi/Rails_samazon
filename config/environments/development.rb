5b20771ed7ee3401726c0aa1bc741ea2878d3a13
KengoKonishi <konitanz627@gmail.com>
2021-06-17 14:27:22 +0000

    05 create login system

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,23 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  Rails.application.configure do
+    # default url
+    config.action_mailer.default_url_options = {
+      protocol: 'https',
+      host: "https://615cc6a6fa984c8ab279dc4f5cf4fa8e.vfs.cloud9.us-east-2.amazonaws.com"
+    }
+    # mail setting
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => "konitanz627@gmail.com",
+      :password => "pwnapoaxtahuvurd",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  end
 end
