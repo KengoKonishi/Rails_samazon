b97ce51c3ca0333177da0af20c2d2e6d98bc9b45
KengoKonishi <konitanz627@gmail.com>
2021-06-18 14:47:03 +0000

    aaa

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,24 +9,24 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-    def create
-      super
-    end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
-  
-　 def after_sign_in_path_for(user)
-　   products_path
-　  end
 
-　 def after_sign_out_path_for(user)
-　   root_path
-　 end
-　
-    protected
+   def after_sign_in_path_for(user)
+     products_path
+   end
+ 
+   def after_sign_out_path_for(user)
+     root_path
+   end
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_in_params
